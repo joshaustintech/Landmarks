35d375126e4b69edb5bf82d1c9ebc64e30693ba9
Joshua Austin <83935880+joshaustintech@users.noreply.github.com>
2023-10-17 18:32:51 -0500

    Added "Favorite" functionality

--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -4,7 +4,16 @@ struct LandmarkDetail: View {
     
     var landmark: Landmark
     
+    @Environment(ModelData.self) var modelData
+    
     var body: some View {
+        
+        @Bindable var modelData = modelData
+        
+        var landmarkIndex: Int {
+            modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+        }
+        
         ScrollView {
             
             MapView(coordinate: landmark.locationCoordinate)
@@ -15,8 +24,11 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -150)
             
             VStack(alignment: . leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 
                 HStack {
                     Text(landmark.park)
@@ -42,5 +54,7 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail(landmark: landmarks[0])
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: modelData.landmarks[0])
+        .environment(modelData)
 }
